cef4c98496c5a7b0ba5238a7ade03b13f801b714
Alexander L Kharag <akharag@dmlmac3.rd.unr.edu>
2019-03-28 14:22:28 -0700

    Loads new card in incorrect location

--- Espy/Espy/OutfitMakerViewController.swift
+++ Espy/Espy/OutfitMakerViewController.swift
@@ -18,15 +18,21 @@ class OutfitMakerViewerController: UIViewController{
     @IBOutlet weak var bottomImageView: UIImageView!
     @IBOutlet weak var shoesImageView: UIImageView!
     
-    var i = 0
+    var topIndex = 0
+    var bottomIndex = 0
+    var shoeIndex = 0
     var divisor:CGFloat!
+    
+    var topImages = [UIImage]()
+    var bottomImages = [UIImage]()
+    var shoeImages = [UIImage]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         divisor = (view.frame.width / 2) / 0.61
         
         var loadItems = [Clothing]()
-        var images = [UIImage]()
+        
         
         do{
             loadItems = try loadFromJSONFile(filename: "wardrobe")
@@ -36,21 +42,36 @@ class OutfitMakerViewerController: UIViewController{
         if loadItems.count > 0{
             for i in 0...loadItems.count-1 {
                 if let data = Data(base64Encoded: loadItems[i].image, options: .ignoreUnknownCharacters){
-                    images.append(UIImage(data: data)!)
+                    if loadItems[i].tag == "top"{
+                        topImages.append(UIImage(data: data)!)
+                    }else if loadItems[i].tag == "bottom"{
+                        bottomImages.append(UIImage(data: data)!)
+                    }else if loadItems[i].tag == "shoes"{
+                        shoeImages.append(UIImage(data: data)!)
+                    }else{
+                        print("Image with unknown tag: \(loadItems[i].tag)")
+                    }
                 }
             }
         }
-        while(i < wardrobeItems.count){
-            if loadItems[i].tag == "top" {
-                topImageView.image = images[i]
-            } else if loadItems[i].tag == "bottom"{
-                bottomImageView.image = images[i]
-            } else if loadItems[i].tag == "shoes"{
-                shoesImageView.image = images[i]
-            }
-            i += 1
+     
+        if topImages.isEmpty {
+            print("No TOP image found")
+        }else{
+            topImageView.image = topImages[0]
         }
-        
+
+        if bottomImages.isEmpty {
+            print("No BOTTOM image found")
+        }else{
+            bottomImageView.image = bottomImages[0]
+        }
+
+        if shoeImages.isEmpty {
+            print("No SHOES image found")
+        }else{
+            shoesImageView.image = shoeImages[0]
+        }
     }
     
     @IBAction func panTop(_ sender: UIPanGestureRecognizer) {
@@ -64,10 +85,29 @@ class OutfitMakerViewerController: UIViewController{
         if sender.state == .ended{
             if card.center.x < 50{
                 UIView.animate(withDuration: 0.2,
-                               animations: {card.center = CGPoint(x: self.view.center.x-400, y: self.view.center.y + -200)
+                               animations: {
+                                card.center = CGPoint(x: self.view.center.x-400, y: self.view.center.y + -200)
                                 card.alpha = 0
                 })
-                
+                card.center = CGPoint(x: self.view.center.x, y: self.view.center.y + -230)
+                card.transform = CGAffineTransform.identity
+                card.frame = CGRect(x: self.view.center.x, y: self.view.center.y, width: card.frame.size.width * 0.2, height: card.frame.size.width * 0.2)
+                //load next picture
+                    //check if out of bounds
+                if( topIndex + 1 < topImages.count ){
+                    topIndex += 1
+                    topImageView.image = topImages[topIndex]
+                }else{
+                    print("All Images used")
+                    topIndex = 0
+                    topImageView.image = topImages[0]
+                }
+                UIView.animate(withDuration: 0.2,
+                               animations: {
+                                card.frame = CGRect(x: self.view.center.x, y: self.view.center.y + -230, width: card.frame.size.width / 0.2, height: card.frame.size.width / 0.2)
+                                card.alpha = 1
+                                
+                })
             }else if card.center.x > (view.frame.width - 50){
                 UIView.animate(withDuration: 0.2,
                                animations: {card.center = CGPoint(x: self.view.center.x+400, y: self.view.center.y + -200)
@@ -103,6 +143,11 @@ class OutfitMakerViewerController: UIViewController{
                                animations: {card.center = CGPoint(x: self.view.center.x+400, y: self.view.center.y + 35)
                                 card.alpha = 0
                 })
+                card.center = CGPoint(x: 0, y: self.view.center.y + 35)
+                //load next picture
+                //if()
+                //topImageView.image =
+                UIView.animate(withDuration: 0.2, animations: {card.alpha = 1})
             } else{
                 UIView.animate(withDuration: 0.2, animations: {card.center = CGPoint(x: self.view.center.x, y: self.view.center.y + 30)
                     card.transform = CGAffineTransform.identity
